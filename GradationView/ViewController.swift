b06395a6c44568398dddfa1a926373c7852cfc5c
anthrgrnwrld <anthrgrnwrld@gmail.com>
2016-09-23 19:38:28 +0900

    commit

--- GradationView/ViewController.swift
+++ GradationView/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gradationView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let gradient = CAGradientLayer()
+        gradient.frame = self.view.bounds
+        
+        let color_1 = UIColor(red: 1.0, green: 0.7, blue: 0.7, alpha: 1.0).cgColor
+        let color_2 = UIColor(red: 0.7, green: 1.0, blue: 0.7, alpha: 1.0).cgColor
+        let color_3 = UIColor(red: 0.7, green: 0.7, blue: 1.0, alpha: 1.0).cgColor
+        gradient.colors = [color_1, color_2, color_3]
+        
+        let position_1 = NSNumber(value: 0.0 as Float)
+        let position_2 = NSNumber(value: 0.5 as Float)
+        let position_3 = NSNumber(value: 1.0 as Float)
+        
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.locations = [position_1, position_2, position_3]
+        
+        self.gradationView.layer.insertSublayer(gradient, at: 0)
+
     }
 
     override func didReceiveMemoryWarning() {
